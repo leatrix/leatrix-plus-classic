c830b93407d22c0fefed212be4369fb0da962a5a
leatrix <58888415+leatrix@users.noreply.github.com>
2021-05-26 09:59:10 +0100

    1.13.107

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 1.13.107.alpha.1 (20th May 2021)
+-- 	Leatrix Plus 1.13.107 (26th May 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "1.13.107.alpha.1"
+	LeaPlusLC["AddonVer"] = "1.13.107"
 	LeaPlusLC["RestartReq"] = nil
 
 	-- Get locale table
