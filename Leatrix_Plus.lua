df72c7e3ca35c0acb1d6ce5cc89bc0e8bda8a9c4
leatrix <58888415+leatrix@users.noreply.github.com>
2021-12-08 04:21:29 +0000

    1.14.13.alpha.4

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 1.14.13.alpha.3 (7th December 2021)
+-- 	Leatrix Plus 1.14.13.alpha.4 (8th December 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "1.14.13.alpha.3"
+	LeaPlusLC["AddonVer"] = "1.14.13.alpha.4"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -3485,7 +3485,7 @@
 				where:HookScript("OnEnter", function() 
 					GameTooltip:SetOwner(where, "ANCHOR_NONE")
 					GameTooltip:SetPoint("BOTTOM", where, "TOP", 0, 10)
-					GameTooltip:SetText(tip, nil, nil, nil, nil, true)
+					GameTooltip:SetText(L[tip], nil, nil, nil, nil, true)
 				end)
 				where:HookScript("OnLeave", GameTooltip_Hide)
 			end
