3af1574e6d0299b10431e2fa7f0b5d404aa5feff
leatrix <58888415+leatrix@users.noreply.github.com>
2022-08-31 20:32:34 +0100

    1.14.59.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 1.14.58 (31st August 2022)
+-- 	Leatrix Plus 1.14.59.alpha.1 (31st August 2022)
 ----------------------------------------------------------------------
 
 --	01:Functns, 02:Locks, 03:Restart, 20:Live, 30:Isolated, 40:Player
@@ -19,7 +19,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "1.14.58"
+	LeaPlusLC["AddonVer"] = "1.14.59.alpha.1"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -561,6 +561,7 @@
 		or	(LeaPlusLC["TipNoHealthBar"]		~= LeaPlusDB["TipNoHealthBar"])			-- Tooltip hide health bar
 		or	(LeaPlusLC["EnhanceDressup"]		~= LeaPlusDB["EnhanceDressup"])			-- Enhance dressup
 		or	(LeaPlusLC["EnhanceQuestLog"]		~= LeaPlusDB["EnhanceQuestLog"])		-- Enhance quest log
+		or	(LeaPlusLC["EnhanceQuestTaller"]	~= LeaPlusDB["EnhanceQuestTaller"])		-- Enhance quest taller
 		or	(LeaPlusLC["EnhanceProfessions"]	~= LeaPlusDB["EnhanceProfessions"])		-- Enhance professions
 		or	(LeaPlusLC["EnhanceTrainers"]		~= LeaPlusDB["EnhanceTrainers"])		-- Enhance trainers
 
@@ -7069,104 +7070,118 @@
 
 		if LeaPlusLC["EnhanceQuestLog"] == "On" then
 
-			-- Set increased height of quest log frame and maximum number of quests listed
-			local tall, numTallQuests = 73, 21
+			if LeaPlusLC["EnhanceQuestTaller"] == "On" then
 
-			-- Make the quest log frame double-wide
-			UIPanelWindows["QuestLogFrame"] = {area = "override", pushable = 0, xoffset = -16, yoffset = 12, bottomClampOverride = 140 + 12, width = 714, height = 487, whileDead = 1}
+				-- Set increased height of quest log frame and maximum number of quests listed
+				local tall, numTallQuests = 73, 21
 
-			-- Size the quest log frame
-			QuestLogFrame:SetWidth(714)
-			QuestLogFrame:SetHeight(487 + tall)
+				-- Make the quest log frame double-wide
+				UIPanelWindows["QuestLogFrame"] = {area = "override", pushable = 0, xoffset = -16, yoffset = 12, bottomClampOverride = 140 + 12, width = 714, height = 487, whileDead = 1}
 
-			-- Adjust quest log title text
-			QuestLogTitleText:ClearAllPoints()
-			QuestLogTitleText:SetPoint("TOP", QuestLogFrame, "TOP", 0, -18)
+				-- Size the quest log frame
+				QuestLogFrame:SetWidth(714)
+				QuestLogFrame:SetHeight(487 + tall)
 
-			-- Move the detail frame to the right and stretch it to full height
-			QuestLogDetailScrollFrame:ClearAllPoints()
-			QuestLogDetailScrollFrame:SetPoint("TOPLEFT", QuestLogListScrollFrame, "TOPRIGHT", 31, 1)
-			QuestLogDetailScrollFrame:SetHeight(336 + tall)
+				-- Adjust quest log title text
+				QuestLogTitleText:ClearAllPoints()
+				QuestLogTitleText:SetPoint("TOP", QuestLogFrame, "TOP", 0, -18)
 
-			-- Expand the quest list to full height
-			QuestLogListScrollFrame:SetHeight(336 + tall)
+				-- Move the detail frame to the right and stretch it to full height
+				QuestLogDetailScrollFrame:ClearAllPoints()
+				QuestLogDetailScrollFrame:SetPoint("TOPLEFT", QuestLogListScrollFrame, "TOPRIGHT", 31, 1)
+				QuestLogDetailScrollFrame:SetHeight(336 + tall)
 
-			-- Create additional quest rows
-			local oldQuestsDisplayed = QUESTS_DISPLAYED
-			_G.QUESTS_DISPLAYED = _G.QUESTS_DISPLAYED + numTallQuests
-			for i = oldQuestsDisplayed + 1, QUESTS_DISPLAYED do
-				local button = CreateFrame("Button", "QuestLogTitle" .. i, QuestLogFrame, "QuestLogTitleButtonTemplate")
-				button:SetID(i)
-				button:Hide()
-				button:ClearAllPoints()
-				button:SetPoint("TOPLEFT", _G["QuestLogTitle" .. (i-1)], "BOTTOMLEFT", 0, 1)
+				-- Expand the quest list to full height
+				QuestLogListScrollFrame:SetHeight(336 + tall)
+
+				-- Create additional quest rows
+				local oldQuestsDisplayed = QUESTS_DISPLAYED
+				_G.QUESTS_DISPLAYED = _G.QUESTS_DISPLAYED + numTallQuests
+				for i = oldQuestsDisplayed + 1, QUESTS_DISPLAYED do
+					local button = CreateFrame("Button", "QuestLogTitle" .. i, QuestLogFrame, "QuestLogTitleButtonTemplate")
+					button:SetID(i)
+					button:Hide()
+					button:ClearAllPoints()
+					button:SetPoint("TOPLEFT", _G["QuestLogTitle" .. (i-1)], "BOTTOMLEFT", 0, 1)
+				end
+
+				-- Get quest frame textures
+				local regions = {QuestLogFrame:GetRegions()}
+
+				-- Set top left texture
+				regions[3]:SetSize(512, 512)
+				regions[3]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
+				regions[3]:SetTexCoord(0.25, 0.75, 0, 1)
+
+				-- Set top right texture
+				regions[4]:ClearAllPoints()
+				regions[4]:SetPoint("TOPLEFT", regions[3], "TOPRIGHT", 0, 0)
+				regions[4]:SetSize(256, 512)
+				regions[4]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
+				regions[4]:SetTexCoord(0.75, 1, 0, 1)
+
+				-- Hide bottom left and bottom right textures
+				regions[5]:Hide()
+				regions[6]:Hide()
+
+				-- Position and resize abandon button
+				QuestLogFrameAbandonButton:SetSize(110, 21)
+				QuestLogFrameAbandonButton:SetText(ABANDON_QUEST_ABBREV)
+				QuestLogFrameAbandonButton:ClearAllPoints()
+				QuestLogFrameAbandonButton:SetPoint("BOTTOMLEFT", QuestLogFrame, "BOTTOMLEFT", 17, 54)
+
+				-- Position and resize share button
+				QuestFramePushQuestButton:SetSize(100, 21)
+				QuestFramePushQuestButton:SetText(SHARE_QUEST_ABBREV)
+				QuestFramePushQuestButton:ClearAllPoints()
+				QuestFramePushQuestButton:SetPoint("LEFT", QuestLogFrameAbandonButton, "RIGHT", -3, 0)
+
+				-- Add map button
+				local logMapButton = CreateFrame("Button", nil, QuestLogFrame, "UIPanelButtonTemplate")
+				logMapButton:SetText(L["Map"])
+				logMapButton:ClearAllPoints()
+				logMapButton:SetPoint("LEFT", QuestFramePushQuestButton, "RIGHT", -3, 0)
+				logMapButton:SetSize(100, 21)
+				logMapButton:SetScript("OnClick", ToggleWorldMap)
+
+				-- Position and size close button
+				QuestFrameExitButton:SetSize(80, 22)
+				QuestFrameExitButton:SetText(CLOSE)
+				QuestFrameExitButton:ClearAllPoints()
+				QuestFrameExitButton:SetPoint("BOTTOMRIGHT", QuestLogFrame, "BOTTOMRIGHT", -42, 54)
+
+				-- Empty quest frame
+				QuestLogNoQuestsText:ClearAllPoints()
+				QuestLogNoQuestsText:SetPoint("TOP", QuestLogListScrollFrame, 0, -50)
+				hooksecurefunc(EmptyQuestLogFrame, "Show", function()
+					EmptyQuestLogFrame:ClearAllPoints()
+					EmptyQuestLogFrame:SetPoint("BOTTOMLEFT", QuestLogFrame, "BOTTOMLEFT", 20, -76)
+					EmptyQuestLogFrame:SetHeight(487)
+				end)
+
+				-- Show map button (not currently used)
+				local mapButton = CreateFrame("BUTTON", nil, QuestLogFrame)
+				mapButton:SetSize(36, 25)
+				mapButton:SetPoint("TOPRIGHT", -390, -44)
+				mapButton:SetNormalTexture("Interface\\QuestFrame\\UI-QuestMap_Button")
+				mapButton:GetNormalTexture():SetTexCoord(0.125, 0.875, 0, 0.5)
+				mapButton:SetPushedTexture("Interface\\QuestFrame\\UI-QuestMap_Button")
+				mapButton:GetPushedTexture():SetTexCoord(0.125, 0.875, 0.5, 1.0)
+				mapButton:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
+				mapButton:SetScript("OnClick", ToggleWorldMap)
+				mapButton:Hide()
+
+				-- ElvUI fixes
+				if LeaPlusLC.ElvUI then
+					local E = LeaPlusLC.ElvUI
+					if E.private.skins.blizzard.enable and E.private.skins.blizzard.quest then
+						-- Skin map button
+						_G.LeaPlusGlobalMapButton = logMapButton
+						E:GetModule("Skins"):HandleButton(_G.LeaPlusGlobalMapButton)
+					end
+				end
+
 			end
-
-			-- Get quest frame textures
-			local regions = {QuestLogFrame:GetRegions()}
-
-			-- Set top left texture
-			regions[3]:SetSize(512, 512)
-			regions[3]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-			regions[3]:SetTexCoord(0.25, 0.75, 0, 1)
-
-			-- Set top right texture
-			regions[4]:ClearAllPoints()
-			regions[4]:SetPoint("TOPLEFT", regions[3], "TOPRIGHT", 0, 0)
-			regions[4]:SetSize(256, 512)
-			regions[4]:SetTexture("Interface\\AddOns\\Leatrix_Plus\\Leatrix_Plus")
-			regions[4]:SetTexCoord(0.75, 1, 0, 1)
-
-			-- Hide bottom left and bottom right textures
-			regions[5]:Hide()
-			regions[6]:Hide()
-
-			-- Position and resize abandon button
-			QuestLogFrameAbandonButton:SetSize(110, 21)
-			QuestLogFrameAbandonButton:SetText(ABANDON_QUEST_ABBREV)
-			QuestLogFrameAbandonButton:ClearAllPoints()
-			QuestLogFrameAbandonButton:SetPoint("BOTTOMLEFT", QuestLogFrame, "BOTTOMLEFT", 17, 54)
-
-			-- Position and resize share button
-			QuestFramePushQuestButton:SetSize(100, 21)
-			QuestFramePushQuestButton:SetText(SHARE_QUEST_ABBREV)
-			QuestFramePushQuestButton:ClearAllPoints()
-			QuestFramePushQuestButton:SetPoint("LEFT", QuestLogFrameAbandonButton, "RIGHT", -3, 0)
-
-			-- Add map button
-			local logMapButton = CreateFrame("Button", nil, QuestLogFrame, "UIPanelButtonTemplate")
-			logMapButton:SetText(L["Map"])
-			logMapButton:ClearAllPoints()
-			logMapButton:SetPoint("LEFT", QuestFramePushQuestButton, "RIGHT", -3, 0)
-			logMapButton:SetSize(100, 21)
-			logMapButton:SetScript("OnClick", ToggleWorldMap)
-
-			-- Position and size close button
-			QuestFrameExitButton:SetSize(80, 22)
-			QuestFrameExitButton:SetText(CLOSE)
-			QuestFrameExitButton:ClearAllPoints()
-			QuestFrameExitButton:SetPoint("BOTTOMRIGHT", QuestLogFrame, "BOTTOMRIGHT", -42, 54)
-
-			-- Empty quest frame
-			QuestLogNoQuestsText:ClearAllPoints()
-			QuestLogNoQuestsText:SetPoint("TOP", QuestLogListScrollFrame, 0, -50)
-			hooksecurefunc(EmptyQuestLogFrame, "Show", function()
-				EmptyQuestLogFrame:ClearAllPoints()
-				EmptyQuestLogFrame:SetPoint("BOTTOMLEFT", QuestLogFrame, "BOTTOMLEFT", 20, -76)
-				EmptyQuestLogFrame:SetHeight(487)
-			end)
-
-			-- Show map button (not currently used)
-			local mapButton = CreateFrame("BUTTON", nil, QuestLogFrame)
-			mapButton:SetSize(36, 25)
-			mapButton:SetPoint("TOPRIGHT", -390, -44)
-			mapButton:SetNormalTexture("Interface\\QuestFrame\\UI-QuestMap_Button")
-			mapButton:GetNormalTexture():SetTexCoord(0.125, 0.875, 0, 0.5)
-			mapButton:SetPushedTexture("Interface\\QuestFrame\\UI-QuestMap_Button")
-			mapButton:GetPushedTexture():SetTexCoord(0.125, 0.875, 0.5, 1.0)
-			mapButton:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
-			mapButton:SetScript("OnClick", ToggleWorldMap)
-			mapButton:Hide()
 
 			-- Show quest levels
 			hooksecurefunc("QuestLog_Update", function()
@@ -7209,21 +7224,12 @@
 				end
 			end)
 
-			-- ElvUI fixes
-			if LeaPlusLC.ElvUI then
-				local E = LeaPlusLC.ElvUI
-				if E.private.skins.blizzard.enable and E.private.skins.blizzard.quest then
-					-- Skin map button
-					_G.LeaPlusGlobalMapButton = logMapButton
-					E:GetModule("Skins"):HandleButton(_G.LeaPlusGlobalMapButton)
-				end
-			end
-
 			-- Create configuration panel
 			local EnhanceQuestPanel = LeaPlusLC:CreatePanel("Enhance quest log", "EnhanceQuestPanel")
 
 			LeaPlusLC:MakeTx(EnhanceQuestPanel, "Settings", 16, -72)
-			LeaPlusLC:MakeCB(EnhanceQuestPanel, "EnhanceQuestLevels", "Show quest levels", 16, -92, false, "If checked, quest levels will be shown.")
+			LeaPlusLC:MakeCB(EnhanceQuestPanel, "EnhanceQuestTaller", "Larger quest log frame", 16, -92, true, "If checked, the quest log frame will be larger.")
+			LeaPlusLC:MakeCB(EnhanceQuestPanel, "EnhanceQuestLevels", "Show quest levels", 16, -112, false, "If checked, quest levels will be shown.")
 
 			-- Help button hidden
 			EnhanceQuestPanel.h:Hide()
@@ -11634,6 +11640,7 @@
 				LeaPlusLC:LoadVarChk("DressupAnimControl", "On")			-- Dressup animation control
 				LeaPlusLC:LoadVarChk("HideDressupStats", "Off")				-- Hide dressup stats
 				LeaPlusLC:LoadVarChk("EnhanceQuestLog", "Off")				-- Enhance quest log
+				LeaPlusLC:LoadVarChk("EnhanceQuestTaller", "On")			-- Enhance quest log taller
 				LeaPlusLC:LoadVarChk("EnhanceQuestLevels", "On")			-- Enhance quest log quest levels
 				LeaPlusLC:LoadVarChk("EnhanceProfessions", "Off")			-- Enhance professions
 				LeaPlusLC:LoadVarChk("EnhanceTrainers", "Off")				-- Enhance trainers
@@ -11984,6 +11991,7 @@
 			LeaPlusDB["DressupAnimControl"]		= LeaPlusLC["DressupAnimControl"]
 			LeaPlusDB["HideDressupStats"]		= LeaPlusLC["HideDressupStats"]
 			LeaPlusDB["EnhanceQuestLog"]		= LeaPlusLC["EnhanceQuestLog"]
+			LeaPlusDB["EnhanceQuestTaller"]		= LeaPlusLC["EnhanceQuestTaller"]
 			LeaPlusDB["EnhanceQuestLevels"]		= LeaPlusLC["EnhanceQuestLevels"]
 			LeaPlusDB["EnhanceProfessions"]		= LeaPlusLC["EnhanceProfessions"]
 			LeaPlusDB["EnhanceTrainers"]		= LeaPlusLC["EnhanceTrainers"]
@@ -13871,6 +13879,7 @@
 				LeaPlusDB["EnhanceDressup"] = "On"				-- Enhance dressup
 				LeaPlusDB["HideDressupStats"] = "On"			-- Hide dressup stats
 				LeaPlusDB["EnhanceQuestLog"] = "On"				-- Enhance quest log
+				LeaPlusDB["EnhanceQuestTaller"] = "On"			-- Enhance quest log taller
 				LeaPlusDB["EnhanceQuestLevels"] = "On"			-- Enhance quest log quest levels
 				LeaPlusDB["EnhanceProfessions"] = "On"			-- Enhance professions
 				LeaPlusDB["EnhanceTrainers"] = "On"				-- Enhance trainers
