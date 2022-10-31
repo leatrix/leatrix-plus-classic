8b512463383887f329bd2d317a8a24514758cd78
leatrix <58888415+leatrix@users.noreply.github.com>
2022-10-31 21:11:03 +0000

    1.14.67.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 1.14.66 (26th October 2022)
+-- 	Leatrix Plus 1.14.67.alpha.1 (31st October 2022)
 ----------------------------------------------------------------------
 
 --	01:Functns, 02:Locks, 03:Restart, 20:Live, 30:Isolated, 40:Player
@@ -19,7 +19,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "1.14.66"
+	LeaPlusLC["AddonVer"] = "1.14.67.alpha.1"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
--- Leatrix_Plus_Flight.lua
+++ Leatrix_Plus_Flight.lua
@@ -1025,6 +1025,7 @@
 					["0.60:0.81:0.64:0.67:0.43:0.25:0.42:0.16"] = 852, -- Gadgetzan, Theramore, Auberdine, Rut'theran Village
 					["0.60:0.81:0.64:0.67:0.40:0.51:0.43:0.25"] = 763, -- Gadgetzan, Theramore, Nijel's Point, Auberdine
 					["0.60:0.81:0.64:0.67:0.40:0.51:0.43:0.25:0.55:0.21:0.65:0.23"] = 1043, -- Gadgetzan, Theramore, Nijel's Point, Auberdine, Moonglade, Everlook
+					["0.60:0.81:0.50:0.76:0.42:0.79:0.31:0.69"] = 362, -- Gadgetzan, Marshal's Refuge, Cenarion Hold, Feathermoon
 
 					-- Alliance: Marshal's Refuge (Un'Goro Crater)
 					["0.50:0.76:0.60:0.81"] = 104, -- Marshal's Refuge, Gadgetzan
