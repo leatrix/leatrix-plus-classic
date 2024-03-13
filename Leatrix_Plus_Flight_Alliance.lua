1396c1067fd5607bc69d7e2f35764e74c281eb8a
leatrix <support@leatrix.com>
2024-03-13 11:20:27 +0000

    1.15.22

--- Leatrix_Plus_Flight_Alliance.lua
+++ Leatrix_Plus_Flight_Alliance.lua
@@ -484,6 +484,7 @@
 				["0.42:0.79:0.60:0.81:0.64:0.67:0.61:0.40:0.46:0.40:0.39:0.40"] = 881, -- Cenarion Hold, Gadgetzan, Theramore, Talrendis Point, Astranaar, Stonetalon Peak
 				["0.42:0.79:0.60:0.81:0.64:0.67:0.40:0.51"] = 669, -- Cenarion Hold, Gadgetzan, Theramore, Nijel's Point
 				["0.42:0.79:0.60:0.81:0.64:0.67:0.61:0.40:0.46:0.40:0.43:0.25:0.42:0.16"] = 961, -- Cenarion Hold, Gadgetzan, Theramore, Talrendis Point, Astranaar, Auberdine, Rut'theran Village
+				["0.42:0.79:0.60:0.81:0.64:0.67:0.61:0.40:0.53:0.26"] = 859, -- Cenarion Hold, Gadgetzan, Theramore, Talrendis Point, Talonbranch Glade
 
 				-- Alliance: Everlook (Winterspring)
 				["0.65:0.23:0.55:0.21"] = 122, -- Everlook, Moonglade
